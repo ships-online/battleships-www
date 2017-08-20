fdb3adf00be5c7e7b71bb2c24b3813268d4ac181
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-08-20 10:43:17 +0200

    Improved error handling.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,32 @@
 import Battleships from 'battleships-core/src/game';
 import NotificationView from 'battleships-ui-vanilla/src/notificationview';
-
 import './main.scss';
 
 const gameId = window.location.hash.split( '#' )[ 1 ];
 const element = document.querySelector( '#game' );
 const notificationWrapper = document.querySelector( '.notification-wrapper' );
 
+if ( !gameId ) {
+	createGame();
+} else {
+	Battleships.join( gameId )
+		.then( game => initGame( game ) )
+		.catch( error => showGameOverScreen( error ) );
+}
+
 function createGame() {
-	Battleships.create()
-		.then( initGame )
-		.catch( ( error ) => showGameOverScreen( error ) );
+	Battleships.create( 10, { 2: 1 } )
+		.then( game => initGame( game ) )
+		.catch( error => showGameOverScreen( error ) );
+}
+
+function initGame( game ) {
+	const notification = new NotificationView( game );
+
+	notificationWrapper.appendChild( notification.render() );
+	game.renderGameToElement( element );
+
+	return game.start();
 }
 
 function showGameOverScreen( error ) {
@@ -20,28 +36,10 @@ function showGameOverScreen( error ) {
 	button.addEventListener( 'click', () => {
 		history.pushState( '', document.title, window.location.pathname + window.location.search );
 		element.innerHTML = '';
-		notificationWrapper.innerHTML = '';
 		createGame();
 	} );
 
+	notificationWrapper.innerHTML = '';
 	element.innerHTML = 'This game is not available because: ' + error;
 	element.appendChild( button );
-}
-
-function initGame( game ) {
-	const notification = new NotificationView( game );
-
-	notificationWrapper.appendChild( notification.render() );
-	game.renderGameToElement( element );
-	game.start();
-
-	return game;
-}
-
-if ( !gameId ) {
-	createGame();
-} else {
-	Battleships.join( gameId )
-		.then( initGame )
-		.catch( error => showGameOverScreen( error ) );
 }
