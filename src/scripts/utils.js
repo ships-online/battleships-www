b7be76ee70f369c826bad23f3d2261a1d297c033
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-06-11 19:57:21 +0200

    Changed setting tooltip.

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -107,12 +107,9 @@ export function start( socketUrl, mainEl, gameEl, gameId ) {
 		let isDisabled = game.status !== 'available' || game.interestedPlayersNumber > 0 || game.player.isReady;
 		let tooltip = '';
 
-		if ( game.status !== 'available' ) {
+		if ( game.status !== 'available' || game.player.isReady ) {
 			isDisabled = true;
-			tooltip = 'Cannot change game settings<br>after the game has started';
-		} else if ( game.player.isReady ) {
-			isDisabled = true;
-			tooltip = 'Cannot change game settings<br>when you are ready for the battle';
+			tooltip = 'Cannot change game settings<br>when you are ready';
 		} else if ( game.interestedPlayersNumber > 0 ) {
 			isDisabled = true;
 			tooltip = 'Cannot change game settings<br>while there are interested players';
