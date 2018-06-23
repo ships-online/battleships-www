f83d7c633e5ae4cb8ae84937887de477fe754670
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-06-23 16:39:14 +0200

    Improved code style.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 /* global SOCKET_URL */
 
 import { start, getGameId } from './utils';
+import { settings } from 'battleships-ui-vue/src/utils';
 
 import 'battleships-theme/src/styles/index.css';
 import '../styles/main.css';
 
 const mainEl = document.querySelector( 'main' );
 const gameId = getGameId();
-import { settings } from 'battleships-ui-vue/src/utils';
 
 start( SOCKET_URL, mainEl, gameId || settings.get( 'gameSettings' ) );
